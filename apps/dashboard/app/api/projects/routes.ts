c2349b8100b34298ff9a514e0f6b4a9689b79304
Parker Freestone <parkerfreestone@gmail.com>
2025-08-13 12:45:56 -0600

    :x: Removed api package for now

--- apps/dashboard/app/api/projects/routes.ts
+++
@@ -1,9 +0,0 @@
-const coerceDates = (d?: string | Date | null) =>
-  d ? (d instanceof Date ? d : new Date(d)) : null;
-
-export const POST = async (req: Request) => {
-  try {
-    const json = await req.json();
-
-    
-};
